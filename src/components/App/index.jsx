b4fc24f5bc2419d278233d1397b628259052218e
remi208 <s208@tmm-sapr.org>
2019-01-04 17:53:45 +0200

    redux installed

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 import ArticleList from '../ArticleList/'
 import data from '../../data'
+import './styles.css'
 
 class App extends Component {
   constructor(){
@@ -20,10 +21,16 @@ class App extends Component {
 
   render(){
     return(
-      <div>
+      <div className="app">
         <h1>Main title</h1>
         <button onClick={this.revertList.bind(this)}>Revert</button>
         <ArticleList list={this.state.reverse ? data.slice().reverse() : data} reverse={this.state.reverse}/>
+        <div className="name">
+          <div className="name__first">{this.props.firstName}</div>
+          <input type="text" placeholder="first"/>
+          <div className="name__second">{this.props.secondName}</div>
+          <input type="text" placeholder="second"/>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,55 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {createStore} from 'redux';
+import {connect, Provider} from 'react-redux';
 
 import App from './components/App/'
 
+const initialState = {
+  firstName: 'brotherman',
+  secondName: 'Bill'
+}
+
+const rootReducer = (state = initialState, action) => {
+  return state;
+}
+
+// function reducer(state, action){
+//   switch (action.type){
+//     case 'CHANGE_NAME':
+//       return {
+//         ...state,
+//         firstName: action.payload
+//       }
+//   }
+//   return state;
+// }
+
+const store = createStore(rootReducer, initialState);
+
+
+const CHANGE_NAME = 'CHANGE_NAME';
+
+const changeFirstName = {
+  type: CHANGE_NAME,
+  payload: null
+}
+
+const mapStateToProps = (state) => {
+  console.log(state)
+  return {
+    firstName: state.firstName,
+    secondName: state.secondName
+  }
+}
+
+const WrappedApp = connect(mapStateToProps)(App);
+
+store.dispatch(changeFirstName);
+
 render(
-  <App />,
+  <Provider store={store}>
+    <WrappedApp />
+  </Provider>,
   document.getElementById('root')
 );
